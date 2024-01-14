0310d122fb5b6c27cd4af96cd06253572bfb2813
Miguel Moratilla Vega <mimove14@gmail.com>
2024-01-14 16:32:53 +0100

    [ADDED] Main README.md updated to include links to Fundamentals

--- 01_Code/02_Event_Driven_Architecture/01_VMs/startup-script.sh
+++ 01_Code/02_Event_Driven_Architecture/01_VMs/startup-script.sh
@@ -13,4 +13,4 @@ sudo apt install -y python3-pip
 # Install requirements
 git clone https://github.com/jabrio/Cloud_Computing_EDEM_2024.git
 pip3 install -r ./Cloud_Computing_EDEM_2024/01_Code/02_Event_Driven_Architecture/01_VMs/requirements.txt
-sudo mv ./Cloud_Computing_EDEM_2024 /home/<YOUR_USER>
+sudo mv ./Cloud_Computing_EDEM_2024 /home/mimove14
